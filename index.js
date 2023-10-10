938bf17015b4452fa6387a97eeed3d9965f7f8b5
John Savin <johnsavin@proton.me>
2023-10-10 16:44:37 -0400

    demo of connecting to a database

---
+++ index.js
@@ -0,0 +1,18 @@
+// import tools from pg library
+// import { Client } from "pg"
+import pg from "pg"
+const { Client } = pg
+// import connection string from credentials file / secret file
+import { connectionURI } from "./creds.js"
+
+// create client connection to database
+const client = new Client ({
+    connectionString: connectionURI, 
+})
+// open connection
+await client.connect()
+// run query
+const time = await client.query ("SELECT NOW()")
+console.log(time)
+// close connection
+client.end()
